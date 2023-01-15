60852a97be656b19d173afc5857fa958dbb285a1
johnwhitefalcon <87903601+johnwhitefalcon@users.noreply.github.com>
2023-01-15 20:59:10 +1100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,39 @@
-const express = require('express')
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+
 const app = express();
 
 app.use(express.static('public'))
 
-app.listen(3000);
+const url = 'mongodb+srv://johnwhitefalcon:Flow8404@cluster0.ufllo.mongodb.net/mongdbase1?retryWrites=true&w=majority';
+
+app.get('/api/endpoint', (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Error connecting to MongoDB Atlas' });
+    } else {
+      console.log('Successfully connected to MongoDB Atlas');
+      const db = client.db('mongdbase1');
+      const collection = db.collection('mongcollect1');
+      collection.find({}).toArray((err, docs) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: 'Error fetching data from MongoDB Atlas' });
+        } else {
+          console.log('Successfully fetched data from MongoDB Atlas');
+          const data = res.status(200).json(docs);
+          res.sendFile(path.join(__dirname, '/'), { data });
+        }
+        client.close();
+      });
+    }
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
