a8555f2e613e707584fdd4f1e49e7421e6c25636
johnwhitefalcon <87903601+johnwhitefalcon@users.noreply.github.com>
2023-03-05 19:54:02 +1100

    Add files via upload

---
+++ page4.js
@@ -0,0 +1,37 @@
+
+
+import React from 'react';
+import Image from 'next/image';
+import { useState } from 'react';
+import { Button, Input } from 'antd';
+import { useForm } from "react-hook-form";
+import Link from 'next/link';
+
+
+export default function pg4(){
+
+
+return (
+
+    <div className="text-white text-3xl font-extrabold bg-center bg-cover h-screen bg-fixed justify-center items-center flex" >
+<div className="space-y-5 fixed z-10">
+heres johnny
+
+</div>
+
+<div>
+    <img src="https://images.unsplash.com/photo-1677321185756-b9708a22b90e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60"/>
+
+</div>
+
+    </div>
+
+)
+
+
+}
+
+
+
+
+
